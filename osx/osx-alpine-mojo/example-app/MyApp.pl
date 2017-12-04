014dbe403e16fa515b3de563bcc7cfa3c5ac3649
Kiel Stirling <kielstirling@gmail.com>
2017-12-04 22:32:56 +1100

    mojo api container

--- osx/osx-alpine-mojo/example-app/MyApp.pl
+++ osx/osx-alpine-mojo/example-app/MyApp.pl
@@ -5,37 +5,43 @@ use Mojo::Pg;
 use feature 'say';
 use Data::Dumper 'Dumper';
 
-app->config(hypnotoad => {
-	listen => ['https://*:443'],
-	pid_file => '/dev/null',
-	proxy => 1,
-	workers => 5
-});
+app->config(
+    hypnotoad => {
+        listen   => ['https://*:443'],
+        pid_file => '/dev/null',
+        proxy    => 1,
+        workers  => 5
+    }
+);
 
 my $pg = Mojo::Pg->new('postgresql://mic:mic@/postgres');
 
 get '/' => sub {
-	my $c = shift;
+    my $c = shift;
 
-	$c->render( 
-		text => encode_json({
-			"get" => 'alive',
-		}),
-	);
+    $c->render(
+        text => encode_json(
+            {
+                "get" => 'alive',
+            }
+        ),
+    );
 };
 
 post '/' => sub {
-	my $c = shift;
-	my $log = $c->app->log;
+    my $c   = shift;
+    my $log = $c->app->log;
 
-	$log->info( "Working in client" );
-	$log->info( 'test: ' . $c->param( 'test' ) );
+    $log->info("Working in client");
+    $log->info( 'test: ' . $c->param('test') );
 
-	$c->render( 
-		text => encode_json({
-			"post" => 'alive',
-		}),
-	);
+    $c->render(
+        text => encode_json(
+            {
+                "post" => 'alive',
+            }
+        ),
+    );
 };
 
 return app;
